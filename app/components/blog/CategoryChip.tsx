ce8b5d6138aaa2fc209cc3314b46af6119d18da4
Jakub Grzymisławski <thegomel10@gmail.com>
2024-08-03 23:41:22 +0200

    Basic nextjs and mdx configuration, create new CategoryChip component

---
+++ app/components/blog/CategoryChip.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CategoryChip = (props: { categoryName: string }) => {
+  return (
+    <div>
+      <div className="py-1 px-2 lg:px-3 rounded bg-black-500">
+        <p className="text-sm lg:text-base text-gray-100">{props.categoryName}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CategoryChip;
