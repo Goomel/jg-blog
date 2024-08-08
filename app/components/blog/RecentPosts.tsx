3f90386739bce894f9b2ee09b2dbc3aa1a180d7c
Jakub Grzymisławski <thegomel10@gmail.com>
2024-08-08 22:27:53 +0200

    Change RecentPosts grid

--- app/components/blog/RecentPosts.tsx
+++ app/components/blog/RecentPosts.tsx
@@ -3,13 +3,43 @@ import PostLink from './PostLink';
 import { PostData } from '../../lib/types';
 
 const RecentPosts = ({ posts }: { posts: PostData[] }) => {
-  return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-6">
-      {posts.map((post: PostData) =>
-        post.metadata ? <PostLink key={post.slug} post={post} /> : null,
-      )}
-    </div>
+  const sortedPosts = posts.sort(
+    (a, b) =>
+      new Date(b.metadata.publishedAt).getTime() - new Date(a.metadata.publishedAt).getTime(),
   );
+
+  const recentPosts = sortedPosts.slice(0, 5);
+
+  if (posts.length < 5) {
+    return (
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-6">
+        {recentPosts
+          .slice(0, 2)
+          .map((post: PostData) =>
+            post.metadata ? <PostLink key={post.slug} post={post} /> : null,
+          )}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 lg:gap-8 mb-10 lg:mb-16">
+          {recentPosts
+            .slice(0, 2)
+            .map((post: PostData) =>
+              post.metadata ? <PostLink key={post.slug} post={post} /> : null,
+            )}
+        </div>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 lg:gap-8">
+          {recentPosts
+            .slice(2, 5)
+            .map((post: PostData) =>
+              post.metadata ? <PostLink key={post.slug} post={post} /> : null,
+            )}
+        </div>
+      </div>
+    );
+  }
 };
 
 export default RecentPosts;
