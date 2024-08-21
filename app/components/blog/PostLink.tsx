d7cf7d6ba9ab5140dce4a23d096ea9d388031608
Jakub Grzymisławski <thegomel10@gmail.com>
2024-08-21 23:00:32 +0200

    Add prettier-plugin-tailwindcss, small PostLink styles changes

--- app/components/blog/PostLink.tsx
+++ app/components/blog/PostLink.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import CategoryChip from './CategoryChip';
+import ArrowRight from '@/public/icons/arrow-right.svg';
 import { Post } from '@/app/lib/types';
 
 const PostItem = ({
@@ -10,16 +11,16 @@ const PostItem = ({
   post: Post;
 }) => {
   return (
-    <Link href={`/blog/${slug}`} className="w-full">
-      <div className="relative w-full aspect-[3/2] rounded-md overflow-hidden mb-4 lg:mb-6">
+    <Link href={`/blog/${slug}`} className="group w-full">
+      <div className="relative aspect-[3/2] w-full overflow-hidden rounded-t-md">
         <Image src={thumbnail} alt="" fill className="object-cover" />
       </div>
-      <div className="flex flex-col gap-4 ">
+      <div className="relative flex flex-col gap-4 px-3 py-4 lg:px-4 lg:py-6">
         <div className="flex items-center gap-3 lg:gap-4">
           <CategoryChip categoryName={category} />
           <span>{publishedAt}</span>
         </div>
-        <p className="font-heading font-medium text-xl sm:text-2xl lg:text-3xl">{title}</p>
+        <p className="font-heading text-xl font-medium sm:text-2xl lg:text-3xl">{title}</p>
         <p>{excerpt}</p>
       </div>
     </Link>
